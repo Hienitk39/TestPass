1d83b0271c7adbb4ce6f001a77bae405536d2d06
Le Nguyen Minh Hien <hienitk39@yahoo.com>
2022-12-10 13:51:16 +0700

    Add 1 h1 element to App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   // jsx
   return (
     <div className='App'>
+      <h1>Create by Hienlnm</h1>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
